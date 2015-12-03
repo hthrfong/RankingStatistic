d662f9b9bc72107e807d072943d3390bc4197318
Heather Fong <hfong@cita.utoronto.ca>
2015-12-03 15:06:22 -0500

    added alternate binom expansion calcs

--- rankingstatistic_3.py
+++ rankingstatistic_3.py
@@ -16,44 +16,70 @@ def incomplete_Gamma(a,x):
     incomplete_G = gamma(a) * gammaincc(a,x)
     return incomplete_G
 
-def log_y(rho,t,N,approx):
+def log_y(rho,t,N,approx,alt):
+    # Take the logarithm of the summation term
     if approx==False:
-        return lambda x: -0.5*rho**2*(1-t**2) + (gammaln(N)-(gammaln(x+1)+gammaln(N-x))) + x*np.log(rho*t/np.sqrt(2)) + np.log(incomplete_Gamma((N-x)/2,0.5*(rho*t)**2))
+        if alt==True:
+            return lambda x: (gammaln(N)-(gammaln(x+1)+gammaln(N-x))) + x*np.log(rho*t/np.sqrt(2)) + np.log(incomplete_Gamma((N-x)/2,0.5*(rho*t)**2))
+        if alt==False:
+            return lambda x: (gammaln(N)-(gammaln(x+1)+gammaln(N-x))) - x*np.log(rho*t/np.sqrt(2)) + np.log(incomplete_Gamma((x+1)/2,0.5*(rho*t)**2))
     if approx==True:
-        return lambda x: -0.5*rho**2*(1-t**2) + (gammaln(N)-(gammaln(x+1)+gammaln(N-x))) + x*np.log(rho*t/np.sqrt(2)) + gammaln((N-x)/2)
+        if alt==True:
+            return lambda x: (gammaln(N)-(gammaln(x+1)+gammaln(N-x))) + x*np.log(rho*t/np.sqrt(2)) + gammaln((N-x)/2)
+        if alt==False:
+            return lambda x: (gammaln(N)-(gammaln(x+1)+gammaln(N-x))) - x*np.log(rho*t/np.sqrt(2)) + gammaln((x+1)/2)
 
-def exp_log_y(rho,t,N,log_peak,approx):
+def exp_log_y(rho,t,N,log_peak,approx,alt):
+    # Take the exponential of the NORMALISED logarithm of the summation term
     if approx==False:
-        return lambda x: np.exp(-0.5*rho**2*(1-t**2) + gammaln(N)-(gammaln(x+1)+gammaln(N-x)) + x*np.log(rho*t/np.sqrt(2)) + np.log(incomplete_Gamma((N-x)/2,0.5*(rho*t)**2)) - log_peak)
+        if alt==True:
+            return lambda x: np.exp(gammaln(N)-(gammaln(x+1)+gammaln(N-x)) + x*np.log(rho*t/np.sqrt(2)) + np.log(incomplete_Gamma((N-x)/2,0.5*(rho*t)**2)) - log_peak)
+        if alt==False:
+            return lambda x: np.exp(gammaln(N)-(gammaln(x+1)+gammaln(N-x)) - x*np.log(rho*t/np.sqrt(2)) + np.log(incomplete_Gamma((x+1)/2,0.5*(rho*t)**2)) - log_peak)
     if approx==True:
-        return lambda x: np.exp(-0.5*rho**2*(1-t**2) + gammaln(N)-(gammaln(x+1)+gammaln(N-x)) + x*np.log(rho*t/np.sqrt(2)) + gammaln((N-x)/2) - log_peak)
+        if alt==True:
+            return lambda x: np.exp(gammaln(N)-(gammaln(x+1)+gammaln(N-x)) + x*np.log(rho*t/np.sqrt(2)) + gammaln((N-x)/2) - log_peak)
+        if alt==False:
+            return lambda x: np.exp(gammaln(N)-(gammaln(x+1)+gammaln(N-x)) - x*np.log(rho*t/np.sqrt(2)) + gammaln((x+1)/2) - log_peak)
 
 NN = np.linspace(1000,10000,50) # number of dimensions
 rho = 10. # nominal SNR
 t = 0.9 # (t_j \cdot t_k)
+alt = True # alt=True --> rho^n*x^(N-n-1), alt=False --> rho^(N-n-1)*x^n (binomial expansion option)
 
-keep = []
+keep = [[],[]] # probability
 
 for N in NN:
+    for alt in [True,False]:
+        n = np.arange(0,N-1) # range of n in summation
 
-    n = np.arange(0,N-1) # range of n in summation
-    idx_100 = np.where((N-n) <= 100) # indices where (N-n) <= 100 (i.e. range where Gamma(a,x) != Gamma(a) ).
-    idx = np.where((N-n) > 100) # indices where (N-n) > 100 (i.e. range where Gamma(a,x) = Gamma(a) ).
+        if alt==True:
+            idx_100 = np.where((N-n) <= 100) # indices where (N-n) <= 100 (i.e. range where Gamma(a,x) != Gamma(a) ).
+            idx = np.where((N-n) > 100) # indices where (N-n) > 100 (i.e. range where Gamma(a,x) = Gamma(a) ).
+        if alt==False:
+            idx_100 = np.where((n+1) <= 100)
+            idx = np.where((n+1) > 100)
 
-    peak_num = max(log_y(rho,t,N,True)(n[idx]))
-    peak_den = max(log_y(rho,1,N,True)(n[idx]))
-                   
-    exp_log_y_num = [exp_log_y(rho,t,N,peak_num,False),exp_log_y(rho,t,N,peak_num,True)] # [exp(log(x0/xm)),...,exp(log(xn/xm))]
-    exp_log_y_den = [exp_log_y(rho,1,N,peak_den,False),exp_log_y(rho,1,N,peak_den,True)] # = [x0/xm,...,xn/xm)]
-
-    num = [np.cumsum(exp_log_y_num[0](n[idx_100])),np.cumsum(exp_log_y_num[1](n[idx]))] # sum{[x0/xm,...,xn/xm]}
-    den = [np.cumsum(exp_log_y_den[0](n[idx_100])),np.cumsum(exp_log_y_den[1](n[idx]))] # sum{[x0,...,xn]} * (1/xm)
+        peak_num = max(log_y(rho,t,N,True,alt)(n[idx]))
+        peak_den = max(log_y(rho,1,N,True,alt)(n[idx]))
     
-    keep.append( ((num[0][-1]+num[1][-1])/(den[0][-1]+den[1][-1])) * (np.exp(peak_num-peak_den)) )
+        exp_log_y_num = [exp_log_y(rho,t,N,peak_num,False,alt),exp_log_y(rho,t,N,peak_num,True,alt)] # [exp(log(x0/xm)),...,exp(log(xn/xm))]
+        exp_log_y_den = [exp_log_y(rho,1,N,peak_den,False,alt),exp_log_y(rho,1,N,peak_den,True,alt)] # = [x0/xm,...,xn/xm)]
 
-plt.plot(NN,keep,label=r"$\rho=$"+str(rho)+"$,(t_j\cdot t_k)=$"+str(t))
+        num = [np.cumsum(exp_log_y_num[0](n[idx_100])),np.cumsum(exp_log_y_num[1](n[idx]))] # sum{[x0/xm,...,xn/xm]}
+        den = [np.cumsum(exp_log_y_den[0](n[idx_100])),np.cumsum(exp_log_y_den[1](n[idx]))] # sum{[x0,...,xn]} * (1/xm)
+    
+        if alt==True:
+            keep[0].append( np.exp(-0.5*rho**2*(1-t**2)) * ((num[0][-1]+num[1][-1])/(den[0][-1]+den[1][-1])) * (np.exp(peak_num-peak_den)) )
+        if alt==False:
+            keep[1].append( np.exp(-0.5*rho**2*(1-t**2))  * ((num[0][-1]+num[1][-1])/(den[0][-1]+den[1][-1])) * (np.exp(peak_num-peak_den+(N-1)*np.log(t))) )
+
+plt.plot(NN,keep[0],label="Eq53",linestyle="-",color="red")
+plt.plot(NN,keep[1],label="Eq22",linestyle="--",color="blue")
 plt.yscale('log')
+plt.grid()
 plt.xlabel('N (number of dimensions)')
+plt.title(r"$\rho=$"+str(rho)+"$,(t_j\cdot t_k)=$"+str(t))
 plt.legend()
 plt.ylabel('Probability')
 plt.savefig("rankingstatistic_3.pdf")
